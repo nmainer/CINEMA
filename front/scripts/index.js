909c72939536ea227a2f552c74c6edcbeea809b1
nmainer <mainermnicolas@gmail.com>
2024-04-14 13:07:02 -0300

    hw node finalizado

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,65 +1,9 @@
-/*console.log (tempData);*/
+let functionValor = require("./index2");
 
 
 $.get("https://students-api.up.railway.app/movies" , (data)=>{
-    valor(data);
+    functionValor(data);
 });
-
-function valor (elemento){
-  elemento.forEach(function ( obj ) {
-    
-        let titulo = document.createElement("a");
-        let año = document.createElement("p");
-        let dir = document.createElement("p");
-        let durac = document.createElement("p");
-        let gen= document.createElement("p");
-        let ra = document.createElement("p");
-        let pos = document.createElement("img");
-        let tarjeta = document.createElement("div");
-       
-        
-        titulo.href=
-        titulo.textContent= obj.title;
-        año.innerHTML = obj.year;
-        dir.innerHTML= obj.director;
-        durac.innerHTML= obj.duration;
-        gen.innerHTML= obj.genre;
-        ra.innerHTML= obj.rate;
-        pos.src= obj.poster;
-
-
-        
-        
-        tarjeta.classList.add("tar");
-        titulo.classList.add("tit1");
-        año.classList.add("tit2");
-        dir.classList.add("tit2");
-        durac.classList.add("tit2");
-        gen.classList.add("tit2");
-        ra.classList.add("tit2");
-        pos.classList.add("imgtarjeta");
-
-
-        
-        tarjeta.appendChild(titulo);
-        tarjeta.appendChild(pos);
-        tarjeta.appendChild(año);
-        tarjeta.appendChild(dir);
-        tarjeta.appendChild(durac);
-        tarjeta.appendChild(gen);
-        tarjeta.appendChild(ra);
-        
-        
-        
-        let conteiner = document.querySelector(".contenedor");
-        
-        
-        conteiner.appendChild(tarjeta);
-        
-        });
-
-
-}
 
 
 
---
+++ front/scripts/index2.js
@@ -0,0 +1,60 @@
+
+
+
+function valor (elemento){
+    elemento.forEach(function ( obj ) {
+      
+          let titulo = document.createElement("a");
+          let año = document.createElement("p");
+          let dir = document.createElement("p");
+          let durac = document.createElement("p");
+          let gen= document.createElement("p");
+          let ra = document.createElement("p");
+          let pos = document.createElement("img");
+          let tarjeta = document.createElement("div");
+         
+          
+          titulo.href=
+          titulo.textContent= obj.title;
+          año.innerHTML = obj.year;
+          dir.innerHTML= obj.director;
+          durac.innerHTML= obj.duration;
+          gen.innerHTML= obj.genre;
+          ra.innerHTML= obj.rate;
+          pos.src= obj.poster;
+  
+  
+          
+          
+          tarjeta.classList.add("tar");
+          titulo.classList.add("tit1");
+          año.classList.add("tit2");
+          dir.classList.add("tit2");
+          durac.classList.add("tit2");
+          gen.classList.add("tit2");
+          ra.classList.add("tit2");
+          pos.classList.add("imgtarjeta");
+  
+  
+          
+          tarjeta.appendChild(titulo);
+          tarjeta.appendChild(pos);
+          tarjeta.appendChild(año);
+          tarjeta.appendChild(dir);
+          tarjeta.appendChild(durac);
+          tarjeta.appendChild(gen);
+          tarjeta.appendChild(ra);
+          
+          
+          
+          let conteiner = document.querySelector(".contenedor");
+          
+          
+          conteiner.appendChild(tarjeta);
+          
+          });
+  
+  
+  }
+
+  module.exports= valor;

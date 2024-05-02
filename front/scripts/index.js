7bbc5ea133928fe94db0c591e10e7caa88856dd7
nmainer <mainermnicolas@gmail.com>
2024-05-02 16:11:31 -0300

    correcciones express2 finalizados

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -3,8 +3,8 @@ const {valor}= require("./index2");
 const axios = require("axios");
 
 
-  const promise = axios.get("https://students-api.up.railway.app/movies");
-
+  const promise = axios.get("http://localhost:3000/Movies");
+   
 
 promise.then((res)=>{
 res.data.forEach((movie)=>{

58a4e597abb5ce9663782d1d205e5ee39ed06a09
nmainer <mainermnicolas@gmail.com>
2024-11-18 09:20:46 -0300

    cambios

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,12 +1,20 @@
- module.exports ={
-  mode: 'development',
-  entry:{
-      index:"./scripts/index.js",
-      formulario: "./scripts/formularioMovie.js"
-  } ,
-
-  output: {
-      path: __dirname + "/public",
-      filename: "[name].bundle.js"
-  }
-}
+module.exports = {
+    mode: 'development',
+    entry: {
+      index: './scripts/index.js', 
+      formulario: './scripts/formularioMovie.js'
+    },
+    output: {
+      path: __dirname + '/public', // Salida en la carpeta public
+      filename: '[name].bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'] // Si tienes CSS, asegúrate de que Webpack los procese
+        }
+      ]
+    }
+  };
+  

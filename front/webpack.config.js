99f5fc7a5e723bddbfa4022f0748622985bbd8b4
nmainer <mainermnicolas@gmail.com>
2024-11-18 12:23:34 -0300

    cambios

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,16 +1,14 @@
-const path = require('path');
+module.exports ={
+  entry:{
+      index:"./scripts/index.js",
+      formulario: "./scripts/formularioMovie.js"
+  } ,
 
-module.exports = {
-  mode: 'production',  // Usa 'production' para la versión final
-  entry: {
-    index: './scripts/index.js',  // Archivo de entrada para index.js
-    formulario: './scripts/formularioMovie.js',  // Archivo de entrada para formulario.js
-  },
   output: {
-    path: path.resolve(__dirname, 'public'),  // Los archivos generados se guardarán en 'public'
-    filename: '[name].bundle.js',  // Generará index.bundle.js, formulario.bundle.js, etc.
-  },
-};
+      path: __dirname + "/public",
+      filename: "[name].bundle.js"
+  }
+}
 
 
   
